ebc76536aa8761cbf57d7aa5d369f1c4104a1e62
whatsinmyopsec <kevin.power.97.kp@gmail.com>
2024-03-09 21:07:03 +0000

    clock icon added
    ⌚

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React from "react";
 
-export const onPress = (params) => {
+export { manifest } from "./manifest";
+
+export const onPress = (/** @type {any} */ params) => {
   const d = new Date();
   alert(`${d.toLocaleTimeString()}`);
-  console.log("on press pressed", {params});
-}
+  console.log("on press pressed", { params });
+};
 
-const App = (props) => {
-  console.log("App render", {props});
+const App = (/** @type {any} */ props) => {
+  console.log("App render", { props });
   return (
     <div
       style={{
-        borderRadius: '4px',
-        padding: '2em',
-        backgroundColor: 'red',
-        color: 'white',
+        borderRadius: "4px",
+        padding: "2em",
+        backgroundColor: "red",
+        color: "white",
       }}
     >
+
       <div>hello from webdeck-time</div>
     </div>
-  )
+  );
 };
 
 export default App;
---
+++ src/manifest.js
@@ -0,0 +1,9 @@
+import PATHS, { Icons } from "./utils/icons";
+
+export const manifest = {
+  icons: {
+    [Icons.DEFAULT]: {
+      icon: PATHS.default,
+    },
+  },
+};
